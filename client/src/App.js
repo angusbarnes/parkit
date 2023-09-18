a89d5ba725f13367da3aad11a28074daa67cf998
Angus Barnes <38348730+angusbarnes@users.noreply.github.com>
2023-09-18 16:12:28 +1000

    Learning React Componenet Architecture

--- client/src/App.js
+++ client/src/App.js
@@ -3,73 +3,121 @@ import './App.css';
 import React, { useState, useCallback, useEffect } from 'react';
 import useWebSocket, { ReadyState } from 'react-use-websocket';
 
-function App() {
-  const [socketUrl, setSocketUrl] = useState('ws://localhost:80');
-  const [messageHistory, setMessageHistory] = useState([]);
+function ParkingModal({ isOpen, onClose, onSubmit }) {
+    const [name, setName] = useState('');
+    const [reservationDate, setReservationDate] = useState('');
 
-  const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
+    const handleSubmit = () => {
+        // Perform the booking or reservation logic here
+        onSubmit(name, reservationDate);
+        onClose();
+    };
 
-  useEffect(() => {
-    if (lastMessage !== null) {
-      setMessageHistory((prev) => prev.concat(lastMessage));
-    }
-  }, [lastMessage, setMessageHistory]);
+    return (
+    <div className={`modal ${isOpen ? 'open' : ''}`}>
+        <div className="modal-content">
+        <h2>Book/Reserve Parking Spot</h2>
+        <label>Name:</label>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <label>Reservation Date:</label>
+        <input
+            type="date"
+            value={reservationDate}
+            onChange={(e) => setReservationDate(e.target.value)}
+        />
+        <button onClick={handleSubmit}>Submit</button>
+        <button onClick={onClose}>Close</button>
+        </div>
+    </div>
+    );
+}
 
-  const handleClickChangeSocketUrl = useCallback(
-    () => setSocketUrl('ws://localhost:80'),
-    []
-  );
+function ParkingSpotButton({ parkingId, onClickHandler }) {
 
-  const handleClickSendMessage = useCallback(() => sendMessage(JSON.stringify({test: 'value'})), []);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const connectionStatus = {
-    [ReadyState.CONNECTING]: 'Connecting',
-    [ReadyState.OPEN]: 'Open',
-    [ReadyState.CLOSING]: 'Closing',
-    [ReadyState.CLOSED]: 'Closed',
-    [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
-  }[readyState];
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Hello, World! From JSX</p>
-        <button>Test Button</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const handleButtonClick = () => {
+      setIsModalOpen(true);
+    };
+  
+    const handleModalClose = () => {
+      setIsModalOpen(false);
+    };
+  
+    return (
+        <div>
+        <button onClick={handleButtonClick}>Book/Reserve Parking Spot {parkingId}</button>
+        <ParkingModal
+          isOpen={isModalOpen}
+          onClose={handleModalClose}
+          onSubmit={(name, reservationDate) => {
+            // Handle booking/reservation logic here and pass data to your parent component or API
+            console.log(`Booking for Parking Spot ${parkingId}: Name - ${name}, Date - ${reservationDate}`);
+            onClickHandler(parkingId)
+          }}
+        />
+      </div>
+    );
+  }
+  
+  function ParkingSpotList({ numberOfSpots, handleParkingSpotClick }) {
+    // Create an array of parking spot IDs based on the numberOfSpots configuration
+    const parkingSpotIds = Array.from({ length: numberOfSpots }, (_, index) => index);
+  
+    return (
       <div>
-      <button onClick={handleClickChangeSocketUrl}>
-        Click Me to change Socket Url
-      </button>
-      <p></p>
-      <button
-        onClick={handleClickSendMessage}
-        disabled={readyState !== ReadyState.OPEN}
-      >
-        Click Me to send 'Hello'
-      </button>
-      <p></p>
-      <span>The WebSocket is currently {connectionStatus}</span>
-      {lastMessage ? <span>Last message: {lastMessage.data}</span> : null}
-      <ul>
-        {messageHistory.map((message, idx) => (
-          <p key={idx}>{message ? message.data : null}</p>
+        {parkingSpotIds.map((id) => (
+          <ParkingSpotButton
+            key={id}
+            parkingId={id}
+            onClickHandler={handleParkingSpotClick}
+          />
         ))}
-      </ul>
-    </div>
-    </div>
-  );
+      </div>
+    );
+  }
+
+function App() {
+    const [socketUrl, setSocketUrl] = useState('ws://localhost:80');
+    const [messageHistory, setMessageHistory] = useState([]);
+    let cellStates = Array(10).fill(0);
+    const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
+
+        useEffect(() => {
+            if (lastMessage !== null) {
+                setMessageHistory((prev) => prev.concat(lastMessage));
+            }
+        }, [lastMessage, setMessageHistory]);
+
+        const handleClickSendMessage = useCallback((id) => sendMessage(
+            JSON.stringify({type: "UPDATE_CELL_REQUEST", cell_id: id, body: {cell_state: 1}})
+        ), []);
+
+        const connectionStatus = {
+            [ReadyState.CONNECTING]: 'Connecting',
+            [ReadyState.OPEN]: 'Open',
+            [ReadyState.CLOSING]: 'Closing',
+            [ReadyState.CLOSED]: 'Closed',
+            [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+        }[readyState];
+
+    return (
+        <div className="App-header">
+            <div>
+                <p></p>
+                <p></p>
+                <span>The WebSocket is currently {connectionStatus}</span>
+                <ParkingSpotList numberOfSpots={10} handleParkingSpotClick={handleClickSendMessage}/>
+                <ul>
+                    {
+                    messageHistory.map((message, idx) => (
+                        <p key={idx}>{message ? message.data : null}</p>
+                    ))
+                    }
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- server/main.js
+++ server/main.js
@@ -1,60 +1,61 @@
-const express = require('express');
-const WebSocket = require('ws');
-const connection_handler = require('./ws_connection_handler.js')
+const express = require("express");
+const WebSocket = require("ws");
+const connection_handler = require("./ws_connection_handler.js");
 
 const app = express();
 const port = 80;
 
 // Serve the static HTML page
-app.use(express.static('../client/build'));
+app.use(express.static("../client/build"));
 
 const server = app.listen(port, () => {
-  console.log(`Server started on http://localhost:${port}`);
+    console.log(`Server started on http://localhost:${port}`);
 });
 
 // WebSocket server
 const wss = new WebSocket.Server({ server });
 
-const STATE_CODE_EMPTY = 0
-const STATE_CODE_FULL = 1
-const STATE_CODE_RESERVED = 3
+const STATE_CODE_EMPTY = 0;
+const STATE_CODE_FULL = 1;
+const STATE_CODE_RESERVED = 3;
 
 let cellStates = Array(10).fill(STATE_CODE_EMPTY);
 
-wss.on('connection', (ws, req) => {
-  // Send the current state to the newly connected client
-  ws.send(JSON.stringify({ type: 'INITIAL_STATE', data: cellStates }));
+wss.on("connection", (ws, req) => {
+    // Send the current state to the newly connected client
+    ws.send(JSON.stringify({ type: "INITIAL_STATE", data: cellStates }));
 
-  const clientIP = req.connection.remoteAddress;
-  console.log(`Client connected from IP: ${clientIP}`);
-  
-  // Handle messages from clients
-  ws.on('message', (message) => {
-    
-    const { type, cell_id, body } = JSON.parse(message);
-    console.log(message.toString())
-    
-    if (type === 'CELL_EVENT_UPDATED') {
+    const clientIP = req.connection.remoteAddress;
+    console.log(`Client connected from IP: ${clientIP}. Client was sent INITIAL_STATE package`);
 
-      const cellIndex = parseInt(cell_id);
-      
-      const { cell_state } = body;
-      
-      if (cellIndex >= 0 && cellIndex < cellStates.length) {
-        console.log(`Park ${cellIndex} updated with state ${cell_state}`)
-        cellStates[cellIndex] = cell_state;
-        // Broadcast the updated state to all connected clients
-        wss.clients.forEach((client) => {
+    // Handle messages from clients
+    ws.on("message", (message) => {
+        const { type, cell_id, body } = JSON.parse(message);
 
-          if (client.readyState === WebSocket.OPEN) {
-            
-            client.send(JSON.stringify({ type: 'CELL_STATE_CHANGE', data: cellStates }));
-            
-          }
-        });
-      }
-    } else if (type === 'POLL_STATE') {
-      ws.send(JSON.stringify({type: 'CURRENT_CELL_STATES', cells: cellStates}))
-    }
-  });
+        console.log(message.toString());
+
+        if (type === "UPDATE_CELL_REQUEST") {
+            const cellIndex = parseInt(cell_id);
+
+            const { cell_state } = body;
+
+            if (cellIndex >= 0 && cellIndex < cellStates.length) {
+                console.log(`Park ${cellIndex} updated with state ${cell_state}`);
+                cellStates[cellIndex] = cell_state;
+                // Broadcast the updated state to all connected clients
+                wss.clients.forEach((client) => {
+                    if (client.readyState === WebSocket.OPEN) {
+                        client.send(
+                            JSON.stringify({ type: "STATE_UPDATE", data: cellStates })
+                        );
+                    }
+                });
+            }
+        } 
+        else if (type === "POLL_STATE") {
+            ws.send(
+                JSON.stringify({ type: "CURRENT_CELL_STATES", cells: cellStates })
+            );
+        }
+    });
 });
